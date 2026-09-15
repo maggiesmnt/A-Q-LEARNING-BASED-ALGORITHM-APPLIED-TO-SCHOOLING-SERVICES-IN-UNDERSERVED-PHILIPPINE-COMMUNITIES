af865ca5de5dcdde4766ff92fd87da7ad41d29a2
maggiesmnt <156593685+maggiesmnt@users.noreply.github.com>
2026-09-16 01:41:07 +0800

    Include Laiban QA markers in map boot view

--- app.js
+++ app.js
@@ -75,8 +75,14 @@ document.getElementById("btnLayer").classList.add("on");
 document.getElementById("btnCenter").onclick=function(){map.setView(unit.getLatLng(),13)};
 
 /* boot */
-drawRoads();drawRoute();drawNodes();drawHaz();placeUnit();renderDrive();renderStops();renderHazards();renderHistory();renderAnalysis();
-map.fitBounds(L.latLngBounds(NODES.map(function(n){return [n.lat,n.lng]})).pad(0.12));
+drawRoads();drawRoute();drawNodes();drawQaSitios();drawHaz();placeUnit();renderDrive();renderStops();renderHazards();renderHistory();renderAnalysis();
+var bootBounds=NODES.map(function(n){return [n.lat,n.lng]});
+if(typeof LAIBAN_SITIO_REGISTRY!=="undefined"){
+  LAIBAN_SITIO_REGISTRY.forEach(function(s){
+    if(typeof s.lat==="number"&&typeof s.lng==="number") bootBounds.push([s.lat,s.lng]);
+  });
+}
+map.fitBounds(L.latLngBounds(bootBounds).pad(0.12));
 setTimeout(function(){
   var e=EK["kab|pun"];
   alertShow("Path closed &mdash; Kabayunan \u2192 Pungo",
